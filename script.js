37723bbf94200fd96e22cd5caa6f94f9fe7e0e9f
Saksham Jain <sakshambro730@gmail.com>
2023-11-13 16:52:02 +0530

    Added space bar event for play and pause ....

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("Welcome to Spotify");
 // Initialize the Variables
 let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
+const seekBar = document.getElementById('seek-bar');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
@@ -15,11 +16,11 @@ let songs = [
     {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/10.mp3", coverPath: "covers/10.jpg"},
     {songName: "Dead Wrong", filePath: "songs/11.mp3", coverPath: "covers/11.jpg"},
 ]
 
@@ -62,6 +63,31 @@ const makeAllPlays = ()=>{
     })
 }
 
+
+// Function to toggle play/pause on spacebar press
+function togglePlayPause(event) {
+    if (event.code === 'Space') {
+      if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+      } else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+      }
+    }
+  }
+  
+  // Event listener for spacebar press
+  document.addEventListener('keydown', togglePlayPause);
+  
+
+  
+  
+
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{ 
         makeAllPlays();
@@ -93,18 +119,86 @@ document.getElementById('next').addEventListener('click', ()=>{
     masterPlay.classList.add('fa-pause-circle');
 
 })
+// Function to play the next song
+function playNextSong() {
+    if (songIndex < songs.length - 1) {
+      songIndex++;
+    } else {
+      songIndex = 0;
+    }
+    playSelectedSong();
+  }
 
-document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
-        songIndex = 0
+
+  
+  
+  document.getElementById('previous').addEventListener('click', ()=>{
+      if(songIndex<=0){
+          songIndex = 0
+        }
+        else{
+            songIndex -= 1;
+        }
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+// Function to play the previous song
+function playPreviousSong() {
+    if (songIndex > 0) {
+      songIndex--;
+    } else {
+      songIndex = songs.length - 1;
     }
-    else{
-        songIndex -= 1;
+    playSelectedSong();
+  }
+
+// Event listener for Home button to play the previous song
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'Home') {
+        event.preventDefault();  // Prevent default behavior (scrolling)
+        playPreviousSong();
     }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
+});
+
+// Event listener for End button to play the next song
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'End') {
+        event.preventDefault();  // Prevent default behavior (scrolling)
+        playNextSong();
+    }
+});
+
+
+
+// Function to play the selected song
+function playSelectedSong() {
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
     masterPlay.classList.remove('fa-play-circle');
     masterPlay.classList.add('fa-pause-circle');
-})
+    gif.style.opacity = 1;
+}
+
+seekBar.addEventListener('input', () => {
+    const seekToTime = (seekBar.value / 100) * audioElement.duration;
+    audioElement.currentTime = seekToTime;
+  });
+
+  seekBar.addEventListener('click', (event) => {
+    const clickPosition = (event.offsetX / seekBar.offsetWidth) * 100;
+    const seekToTime = (clickPosition / 100) * audioElement.duration;
+    audioElement.currentTime = seekToTime;
+  });
+
+
+
+
+  
+
+    
